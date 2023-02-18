69258f51f3052f4eaef3c1e5dd45b83e5ab857bb
Ryan <ryancarlson369@gmail.com>
2023-02-18 10:39:21 -0800

    Testing Git Access

--- Dashboard.cydsn/frucd_display.c
+++ Dashboard.cydsn/frucd_display.c
@@ -20,7 +20,7 @@ void calibrateScreen() {
     CyDelay(300);
 }
 
-// Initializes dashboard layout/labels
+// Initializes dashboard layout/label
 /*
 void initDashTemplate() {
     UG_FillScreen(C_BLACK);
